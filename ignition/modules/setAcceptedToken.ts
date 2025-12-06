e0f9cfd7a33da44fcec0a57a72c3eff9f5c44ba2
Ashwin-Selvaraj <ashwin240899@gmail.com>
2025-12-06 13:27:04 +0530

    Added code for the accepted token function

---
+++ ignition/modules/setAcceptedToken.ts
@@ -0,0 +1,21 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const SetAcceptedTokenModule = buildModule("SetAcceptedTokenModule", (m) => {
+  const contractAddress = m.getParameter("contractAddress");
+  const token = m.getParameter("token");
+  const accept = m.getParameter("accept");
+  const priceFeed = m.getParameter("priceFeed");
+  const owner = m.getAccount(0);
+
+  const contract = m.contractAt("SubscriptionManager", contractAddress);
+  m.call(contract, "setAcceptedToken", [token, accept, priceFeed], { from: owner });
+  return { contract };  
+});
+
+export default SetAcceptedTokenModule;
+
+
+// comment for running the module 
+// npx hardhat ignition deploy ./ignition/modules/setAcceptedToken.ts \
+//   --parameters ignition/parameters/setAcceptedToken.json \
+//   --network bscTestnet
